6f5c1f753143a4cc761ffec0ac20a19b6dfaab7c
Fergasi <98353142+Fergasi@users.noreply.github.com>
2022-07-25 12:59:09 -0400

    first step Heroku deployment ready

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import HomePage from "./Pages/HomePage";
+const urlEndpoint = process.env.REACT_APP_URL_ENDPOINT;
+
+function App() {
+  const [clientMessage, setClientMessage] = useState("");
+  const [serverMessage, setServerMessage] = useState("");
+
+  const sendRecieveMessage = async () => {
+    const response = await fetch(`${urlEndpoint}/post-message`, {
+      method: "POST",
+      body: JSON.stringify({ clientMessage }),
+    });
+    const responseJSON = await response.json();
+    setServerMessage(responseJSON.serverMessage);
+  };
+
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Routes>
+          <Route
+            index
+            element={
+              <HomePage
+                clientMessage={clientMessage}
+                setClientMessage={setClientMessage}
+                serverMessage={serverMessage}
+                sendRecieveMessage={sendRecieveMessage}
+              />
+            }
+          />
+        </Routes>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const HomePage = ({
+  clientMessage,
+  setClientMessage,
+  serverMessage,
+  sendRecieveMessage,
+}) => {
+  return (
+    <div>
+      <h2>Home Page</h2>
+      <br />
+      <h3>Client Message: </h3>
+      <div>{clientMessage}</div>
+      <br />
+      <br />
+      <h3>Server Message: </h3>
+      <div>{serverMessage}</div>
+      <br />
+      <br />
+      <label>Client Message Input: </label>
+      <br />
+      <input
+        type='text'
+        value={clientMessage}
+        onChange={(e) => {
+          const dateTime = new Date();
+          const newClientMessage = `Message: ${
+            e.target.value
+          } at time ${dateTime.toString()}`;
+          setClientMessage(newClientMessage);
+        }}
+      ></input>
+      <br />
+      <br />
+      <button
+        id='sendMessageButton'
+        type='submit'
+        onClick={() => {
+          sendRecieveMessage();
+        }}
+      >
+        Submit
+      </button>
+    </div>
+  );
+};
+
+export default HomePage;
